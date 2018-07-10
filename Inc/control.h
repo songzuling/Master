1d0afbddff4ce77f8243bb30c015fdf8cdd40a69
songzuling <1024511362@qq.com>
2018-07-10 22:17:04 +0800

    修改了头文件里面的枚举命名。需要注意的是，所有命令以CMD开头，是谁发送的命令，接着谁的简称

--- Inc/control.h
+++ Inc/control.h
@@ -18,18 +18,18 @@ typedef struct
 }Master_TypeDef;
 
 enum{
-  CELL_NONE = 0U,
-  CELL_RESET,                   /*复位单元体*/
-  CELL_RESET_SYSTEM,            /*复位单元体系统*/
-  CELL_REQUEST_ENABLE,          /*使能请求ID*/
-  CELL_REQUEST_ID,              /*请求分配ID*/
-  CELL_ALLOCATE_ONE_LAYER_DONE, /*分配一层完成*/
-  CELL_GET_ID,                  /*收到ID*/
-  CELL_DELIVER,                 /*出货命令*/
-  CELL_DELIVER_DONE,            /*单元体出货完成*/
+  CMD_NONE = 0U,
+  CMD_MASTER_RESET_CELL,              /*主机复位单元体*/
+  CMD_MASTER_RESET_CELL_SYSTEM,       /*主机复位单元体系统*/
+  CMD_MASTER_REQUEST_ENABLE,          /*主机使能请求ID*/
+  CMD_CELL_REQUEST_ID,                /*单元体请求分配ID*/
+  CMD_MASTER_ALLOCATE_ONE_LAYER_DONE, /*主机分配一层完成*/
+  CMD_MASTER_SEND_ID,                 /*主机发送ID*/
+  CMD_MASTER_DELIVER,                 /*主机出货命令*/
+  CMD_CELL_DELIVER_DONE,              /*单元体出货完成*/
 };
 
-void Master_init(Master_TypeDef *m);
+void Master_Init(Master_TypeDef *m);
 void AllocatingIdAndAddr(Master_TypeDef *m);
 void Deliver(uint8_t row, uint16_t col, uint8_t n);
 void DoNothing(void);
